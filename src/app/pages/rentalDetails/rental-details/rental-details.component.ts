ce87d169638a8ae75814b4ffaab18d46cc1d2b89
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-06-21 10:24:33 -0600

    add getRentalByCode and deleteRentalByCode

--- src/app/pages/rentalDetails/rental-details/rental-details.component.ts
+++ src/app/pages/rentalDetails/rental-details/rental-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RentalDetailsService } from 'src/app/services/rental-details.service';
 
 @Component({
   selector: 'app-rental-details',
@@ -9,6 +10,8 @@ export class RentalDetailsComponent {
 
   isModifyModalOpen : boolean = false;
   isCancelModalOpen : boolean = false;
+
+  constructor(private rs : RentalDetailsService){}
 
   openModifyModal(){
     this.isModifyModalOpen = true;
@@ -26,5 +29,9 @@ export class RentalDetailsComponent {
 
   closeCancelModal(){
     this.isCancelModalOpen = false;
+  }
+
+  cancelRental(code : string){
+    this.rs.deleteRentalByCode(code);
   }
 }
---
+++ src/app/services/rental-details.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RentalDetailsService {
+
+  constructor(private http : HttpClient) { }
+
+  getRentalByCode(code : string){
+    this.http.get(`https://localhost:7220/api/Rental/track/${code}`);
+  }
+
+  deleteRentalByCode(code : string){
+    this.http.delete(`https://localhost:7220/api/Rental/${code}`);
+  }
+
+}
