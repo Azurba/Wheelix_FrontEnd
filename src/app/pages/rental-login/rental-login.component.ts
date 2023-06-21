4f8ec7d84dd150d7d75e71508fdad63bf75d2fb0
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-06-21 11:17:54 -0600

    add form to login

--- src/app/pages/rental-login/rental-login.component.ts
+++ src/app/pages/rental-login/rental-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-rental-login',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./rental-login.component.scss']
 })
 export class RentalLoginComponent {
+
+  loginForm! : FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      rentalCode: [''], 
+      email: [''] 
+    });
+  }
+
+  login(){
+    if(this.loginForm){
+      const rentalCode = this.loginForm.get('rentalCode')?.value;
+      const email = this.loginForm.get('email')?.value;
+      console.log('Rental Code:', rentalCode);
+      console.log('Email:', email);
+    }
+  }
 
 }
